e2c203b7f232f2cea4734d9bdb9dfd3717ba3852
1305997 <andrewrore94@gmail.com>
2015-11-23 14:44:15 +0000

    ftyhfth

---
+++ connect.js
@@ -0,0 +1,32 @@
+/**
+ * Created by 1305997 on 23/11/2015.
+ */
+var mongodb = require('mongodb');
+var http = require('http');
+var port = process.env.PORT || 1337;
+var url = "mongodb://<dbuser>:<dbpassword>@ds057954.mongolab.com:57954/unidatabase";
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+
+            // do some work here with the database.
+
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
+
+}).listen(port);
+
