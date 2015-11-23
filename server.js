d42f4495889e0463c13ec0dfeb8cbfa1ed5540fe
1305997 <andrewrore94@gmail.com>
2015-11-23 14:20:36 +0000

    ftyhfth

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write("This is a test, bitch!")
+    response.write('This is a test, bitch!\n');
     response.end('Hello World\n');
 }).listen(port);
