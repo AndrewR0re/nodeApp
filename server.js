7fcd54faa557c617ffe3cdbc5ad108c9be2e18d8
1305997 <andrewrore94@gmail.com>
2015-11-30 14:41:27 +0000

    ftyhfth

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 require('./twittertest');
+
+var twitter = require('twitter')
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+var client = new twitter({
+    consumer_key: 'YWI08izs4KPKAcbhZPl94CjFL',
+    consumer_secret: '58jzftT7UG3RgZFVJgw3XzXx5DRTP2gacgZDiHjhj1j8t9stA2',
+    access_token_key: '2865779441-jzCOnSwJvSQyV8UUJYYaLszFh8MdPm6SVdGguh1',
+    access_token_secret: 'kFN5UbhoAL8rAwdkqm9NifgUcsoCyffdfKs1WhcwkxoK5'
+});
+
+http.createServer(function(request, response){
+    response.writeHead(200, {'content-type': 'text/plain'});
+
+    client.get('search/tweets',{q:'lolcats'}, function(error, tweets){
+        console.log(tweets);
+    });
+}).listen(port);
--- twittertest.js
+++ twittertest.js
@@ -3,8 +3,6 @@
  */
 
 var twitter = require('twitter');
-var http = require('http')
-var port = process.env.PORT || 1337;
 
 var client = new twitter({
     consumer_key: 'YWI08izs4KPKAcbhZPl94CjFL',
@@ -16,22 +14,3 @@ var client = new twitter({
 client.get('search/tweets',{q: 'lolcat'}, function(error, tweets){
     console.log(tweets);
 });
-
-var twitter = require('twitter')
-var http = require('http')
-var port = process.env.PORT || 1337;
-
-var client = new twitter({
-    consumer_key: 'YWI08izs4KPKAcbhZPl94CjFL',
-    consumer_secret: '58jzftT7UG3RgZFVJgw3XzXx5DRTP2gacgZDiHjhj1j8t9stA2',
-    access_token_key: '2865779441-jzCOnSwJvSQyV8UUJYYaLszFh8MdPm6SVdGguh1',
-    access_token_secret: 'kFN5UbhoAL8rAwdkqm9NifgUcsoCyffdfKs1WhcwkxoK5'
-});
-
-http.createServer(function(request, response){
-    response.writeHead(200, {'content-type': 'text/plain'});
-
-    client.get('search/tweets',{q:'lolcats'}, function(error, tweets){
-        console.log(tweets);
-    });
-}).listen(port);
